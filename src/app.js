76341351d1387c61d5957fa2a403891750747d47
blbandeira <brunolbandeira@gmail.com>
2020-04-07 22:03:42 -0300

    challange accepted

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,84 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url , techs } = request.body
+
+  const repositorie = {
+    id: uuid(), 
+    title,
+    url,
+    techs,
+    likes: 0
+  }
+
+  repositories.push(repositorie)
+
+  return response.json(repositorie)
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params
+  const {title, url, techs} = request.body
+
+  const repositorieIndex = repositories.findIndex(repositorie => repositorie.id == id)
+
+  if (repositorieIndex < 0) {
+    return response.status(400).json({error: 'cannot find repositorie id'})
+  }
+
+  const repositorie = {
+    id,
+    title,
+    url,
+    techs,
+    likes: repositories[repositorieIndex].likes
+  }
+
+  repositories[repositorieIndex] = repositorie
+
+  return response.json(repositorie)
+
 });
 
 app.delete("/repositories/:id", (req, res) => {
-  // TODO
+  const { id } = req.params
+
+  const repositorieIndex = repositories.findIndex(repositorie => repositorie.id == id)
+
+  if (repositorieIndex < 0) {
+    return res.status(400).json({error: 'cannot find repositorie id'})
+  }
+
+  repositories.splice(repositorieIndex, 1)
+
+  return res.status(204).send()
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params
+  
+  const repositorieIndex = repositories.findIndex(repositorie => repositorie.id == id)
+
+  if (repositorieIndex < 0) {
+    return response.status(400).json({error: 'cannot find repositorie id'})
+  }
+
+  const repositorie = {
+    id,
+    title: repositories[repositorieIndex].title,
+    url: repositories[repositorieIndex].url,
+    techs: repositories[repositorieIndex].techs,
+    likes: repositories[repositorieIndex].likes + 1
+  }
+
+  repositories[repositorieIndex] = repositorie
+
+  return response.json(repositorie)
+
 });
 
 module.exports = app;
